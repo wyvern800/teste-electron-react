e0c723d46f1209d9755bc9e7e0850db9ae6db1b5
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2025-02-12 06:26:40 -0300

    fix: minor issues to imports

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,8 @@
 import '@testing-library/jest-dom';
 
 declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R;
-      toHaveStyle(style: object): R;
-    }
+  export interface Matchers<R> {
+    toBeInTheDocument(): R;
+    toHaveStyle(style: object): R;
   }
 }
