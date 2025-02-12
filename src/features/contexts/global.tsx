6a4f77ccb5eb1f1aad58fde17a8a0d3fda49ecaf
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2025-02-12 13:02:17 -0300

    chore: minor refactor

--- src/features/contexts/global.tsx
+++ src/features/contexts/global.tsx
@@ -1,36 +1,29 @@
 import React, { createContext, useState, useContext } from "react";
 import { GlobalContextType } from "../../core/types/global.context.types";
 
+const base = {
+  paginationData: {
+    currentPage: 1,
+    totalPages: 1
+  },
+  pokemonsList: [],
+  selectedPokemon: null,
+  isDarkMode: false,
+  autoCompletionList: []
+}
+
 export const GlobalContext = createContext<{
   data: GlobalContextType;
   setData: React.Dispatch<React.SetStateAction<GlobalContextType>>;
 }>({
-  data: {
-    paginationData: {
-      currentPage: 1,
-      totalPages: 1
-    },
-    pokemonsList: [],
-    selectedPokemon: null,
-    isDarkMode: false,
-    autoCompletionList: []
-  },
+  data: base,
   setData: () => {
     throw new Error("setData function must be overridden");
   }
 });
 
 export default function GlobalContextProvider({ children }: { children: React.ReactNode }) {
-  const [data, setData] = useState<GlobalContextType>({
-    paginationData: {
-      currentPage: 1,
-      totalPages: 1
-    },
-    pokemonsList: [],
-    selectedPokemon: null,
-    isDarkMode: false,
-    autoCompletionList: []
-  });
+  const [data, setData] = useState<GlobalContextType>(base);
 
   return (
     <GlobalContext.Provider value={{ data, setData }}>
